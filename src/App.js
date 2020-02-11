00b4d37b6d21cd3ca31197d43abad23d765be883
Natalie Miller <nataliemiller03@gmail.com>
2020-02-11 14:34:35 -0800

    Intersection observer

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import { Controller, Scene } from "react-scrollmagic";
+import React, { useState } from 'react';
 import './App.css';
 
 import { SceneOne, SceneTwo, SceneThree, SceneFour } from "./components";
+import useIntersect from "./helpers/useIntersect";
+
+
+const Scene = ({ name, children }) => {
+  const [inView, setInView] = useState(false);
+
+  const [ref, entry] = useIntersect({
+    root: null,
+    threshold: 0,
+    rootMargin: '0px 0px 0px 0px',
+    callbackWhenInView: setInView
+  });
+
+
+  return (
+    <div ref={ref} style={{ opacity: inView ? 1 : 0, transition: 'opacity 1s ease' }}>
+      {children}
+    </div>
+  );
+};
+
 
 const App = () => (
     <div className="App">
-      <Controller>
-        <Scene duration={700} pin>
-          {(progress, event) => {
-            return <div><SceneOne /></div>
-          }}
-        </Scene>
-        <div id="trigger" />
-        <Scene triggerElement="#trigger" duration={500} pin>
-          {progress => (
-            <div>
-              <SceneTwo progress={progress} />
-            </div>
-          )}
-        </Scene>
-        <div id="triggerTwo" />
-        <Scene triggerElement="#triggerTwo" triggerHook={.25} duration={500} pin>
-          {progress => (
-            <div>
-              <SceneThree progress={progress} />
-            </div>
-          )}
-        </Scene>
-        <Scene duration={600} pin>
-        {progress => (
-          <div>
-            <SceneFour progress={progress} />
-          </div>
-        )}
-        </Scene>
-      </Controller>
+      <Scene name="Scene 1">
+        <SceneOne />
+      </Scene>
+      <Scene name="Scene 2">
+        <SceneTwo />
+      </Scene>
+      <Scene name="Scene 3">
+        <SceneThree />
+      </Scene>
     </div>
 );
 
---
+++ src/helpers/useIntersect.js
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+
+const useIntersect = ({ root = null, rootMargin, threshold = 0, callbackWhenInView }) => {
+  const [entry, updateEntry] = useState({});
+  const [node, setNode] = useState(null);
+
+  const observer = useRef(null);
+
+  useEffect(
+    () => {
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new window.IntersectionObserver(
+        ([entry]) => {
+          if (entry.intersectionRatio > 0) {
+            callbackWhenInView(true);
+          }
+          
+        }, 
+        {
+          root,
+          rootMargin,
+          threshold
+        }
+      );
+
+      const { current: currentObserver } = observer;
+
+      if (node) currentObserver.observe(node);
+
+      return () => currentObserver.disconnect();
+    },
+    [node, root, rootMargin, threshold]
+  )
+
+  return [setNode, entry];
+};
+
+export default useIntersect;
