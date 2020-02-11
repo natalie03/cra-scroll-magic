c2a345fd3565ac3e7af9dcc046b5ed8e8808e20f
Natalie Miller <nataliemiller03@gmail.com>
2020-02-10 16:36:10 -0800

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from 'react';
-import { useReducer } from 'react';
+
 import './App.css';
 
 import { SceneOne, SceneTwo, SceneThree, SceneFour } from "./components";
 
-const initialState = {inViewport: false};
-
-function reducer(state, action) {
-  console.log(action);
-  switch (action.type) {
-    case 'setInViewport':
-      return {inViewport: true};
-    default:
-      throw new Error();
-  }
-}
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
     <div className="App">
-      <p>Is scene 2 in viewport? {state.inViewport}</p>
       <SceneOne />
       <SceneTwo />
       <SceneThree />
-      <SceneFour dispatch={dispatch} />
+      <SceneFour />
     </div>
   );
 }
