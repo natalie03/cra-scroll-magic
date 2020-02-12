4acdd9cbf6da3928f8fcce6b6c02e4cdfe33d629
Grady Shelton <gradyish@gmail.com>
2020-02-12 09:16:32 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -6,61 +6,43 @@ import useIntersect from "./helpers/useIntersect";
 
 const Scene = ({ name, children }) => {
   const [inView, setInView] = useState(false);
+  const [prevIntersect, setPrevIntersect] = useState(0);
 
-  const [ref, entry] = useIntersect({
+  const [ref, entry, prevRef] = useIntersect({
     root: null,
     threshold: 0,
     rootMargin: "0px 0px 0px 0px",
     callbackWhenInView: setInView,
+    callbackPrevIntersect: setPrevIntersect,
   });
+
+  // setPrevIntersect(prevRef);
 
   return (
     <div
       ref={ref}
       style={{ opacity: inView ? 1 : 0, transition: "opacity 1s ease" }}
     >
+      {/* {console.log(prevIntersect)}
+      {console.log(prevRef)}
+      {console.log(entry.intersectionRatio)} */}
       {children}
     </div>
   );
 };
 
-const IntersectionScene = props => {
-  const [state, toggle] = useState(props.initial || 0);
-
-  const [ref, entry] = useIntersect({
-    threshold: 0,
-  });
-
-  const Component = compToRender[state];
-
-  const componentName = state === 0 ? "FadeBox" : "WidthBox";
-
-  return (
-    <Component {...props} ref={ref} ratio={entry.intersectionRatio}>
-      intersectionRatio: {format(entry.intersectionRatio)}
-      <Button onClick={() => toggle(state === 0 ? 1 : 0)}>
-        Switch to {componentName}
-      </Button>
-    </Component>
-  );
-};
-
-const App = () => {
-  const dimens = useWindowDimens();
-  console.log(dimens);
-  return (
-    <div className="App">
-      <Scene name="Scene 1">
-        <SceneOne />
-      </Scene>
-      <Scene name="Scene 2">
-        <SceneTwo />
-      </Scene>
-      <Scene name="Scene 3">
-        <SceneThree />
-      </Scene>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <Scene name="Scene 1">
+      <SceneOne />
+    </Scene>
+    {/* <Scene name="Scene 2">
+      <SceneTwo />
+    </Scene> */}
+    <Scene name="Scene 3">
+      <SceneThree />
+    </Scene>
+  </div>
+);
 
 export default App;
--- src/helpers/useIntersect.js
+++ src/helpers/useIntersect.js
@@ -1,39 +1,60 @@
 import { useEffect, useRef, useState } from "react";
 
-const useIntersect = ({ root = null, rootMargin, threshold = 0, callbackWhenInView }) => {
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+const useIntersect = ({
+  root = null,
+  rootMargin,
+  threshold = 0,
+  callbackWhenInView,
+  callbackPrevIntersect,
+}) => {
   const [entry, updateEntry] = useState({});
   const [node, setNode] = useState(null);
 
   const observer = useRef(null);
+  const ref = useRef();
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
 
-  useEffect(
-    () => {
-      if (observer.current) observer.current.disconnect();
-
-      observer.current = new window.IntersectionObserver(
-        ([entry]) => {
-          if (entry.intersectionRatio > 0) {
-            callbackWhenInView(true);
-          }
-          
-        }, 
-        {
-          root,
-          rootMargin,
-          threshold
+    observer.current = new window.IntersectionObserver(
+      ([entry]) => {
+        ref.current = entry.intersectionRatio;
+        if (entry.intersectionRatio > 0) {
+          callbackWhenInView(true);
+          callbackPrevIntersect(ref.current);
+          console.log("aloha");
         }
-      );
+        console.log(entry.intersectionRatio);
+        console.log(ref.current);
+        if (
+          entry.interseactionRatio > 0 &&
+          ref.current < entry.intersectionRatio
+        ) {
+          console.log("starting to enter");
+        }
+      },
+      {
+        root,
+        rootMargin,
+        threshold,
+      },
+    );
 
-      const { current: currentObserver } = observer;
+    const { current: currentObserver } = observer;
 
-      if (node) currentObserver.observe(node);
+    if (node) currentObserver.observe(node);
 
-      return () => currentObserver.disconnect();
-    },
-    [node, root, rootMargin, threshold]
-  )
+    return () => currentObserver.disconnect();
+  }, [node, root, rootMargin, threshold]);
 
-  return [setNode, entry];
+  return [setNode, entry, ref.current];
 };
 
 export default useIntersect;
